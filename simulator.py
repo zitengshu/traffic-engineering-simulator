df62c2b40718c6e9bcdeb167f3029ea5a74617dc
Paul Shu <shuziteng1998@gmail.com>
2022-12-14 19:27:58 -0500

    Finish clos topology

--- simulator.py
+++ simulator.py
@@ -16,8 +16,9 @@ def get_flow_labels(graph: nx.Graph, flow_dict: dict) -> dict:
   return labels
 
 def draw_theoretical_traffic(graph: nx.Graph, labels: dict, image_name: str) -> None:
-  pos=nx.spring_layout(graph)
-  nx.draw_networkx(graph, pos=pos)
+  plt.figure(0, figsize=(20,20))
+  pos=nx.get_node_attributes(graph, 'pos')
+  nx.draw_networkx(graph, pos=pos, node_size=600, font_size=8)
   nx.draw_networkx_edge_labels(graph, pos, edge_labels=labels)
   plt.savefig(image_name)
 
@@ -31,6 +32,18 @@ def gen_clos(graph: nx.Graph, num_server: int, num_leaf: int, num_spine: int, nu
   server_list = [f"server{i}" for i in range(num_server)]
   leaf_list = [f"leaf{i}" for i in range(num_leaf)]
   spine_list = [f"spine{i}" for i in range(num_spine)]
+
+  for i in range(num_server):
+    graph.add_node(server_list[i], pos=(2*i, 3))
+
+  for i in range(num_leaf):
+    graph.add_node(leaf_list[i], pos=(4*i+1, 6))
+  
+  for i in range(num_spine):
+    graph.add_node(spine_list[i], pos=(8*i+2, 9))
+
+  graph.add_node("source", pos=(0,0))
+  graph.add_node("sink", pos=(2*num_server,0))
   for spine in spine_list:
     for leaf in leaf_list:
       graph.add_edge(leaf, spine, capacity=random.sample(capacity_list, 1)[0])
@@ -49,12 +62,16 @@ def gen_clos(graph: nx.Graph, num_server: int, num_leaf: int, num_spine: int, nu
     graph.add_edge(sink, "sink", capacity=float('inf'))
   return
 
-if __name__ == '__main__':
+def clos_example() -> None:
   graph = nx.Graph()
-  gen_clos(graph, 16, 8, 4, 2, [5, 10, 20, 40, 100, 200], 4, 2)
+  gen_clos(graph, 16, 8, 4, 2, [5, 10, 20, 40, 100, 200], 3, 2)
   
   flow_value, flow_dict = nx.maximum_flow(graph, "source", "sink")
 
   labels = get_flow_labels(graph, flow_dict)
-  draw_theoretical_traffic(graph, labels, "theoretical-traffic.png")
-  save_graph(graph, "TE-graph.json")
+  draw_theoretical_traffic(graph, labels, "theoretical-traffic-clos.png")
+  save_graph(graph, "TE-graph-clos.json")
+  print(f"theoretical maximum flow is {flow_value}")
+
+if __name__ == '__main__':
+  print("main")
